fe79b6ec2d1d0953d107c196d8cd1d974fbef6cd
Mustafa Syahmi <mussyahmi31@gmail.com>
2021-02-22 15:39:49 +0800

    checkout page

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Product.css";
 import StarIcon from "@material-ui/icons/Star";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -15,14 +32,16 @@ function Product({ title, image, price, rating }) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <StarIcon />
+              <p>
+                <StarIcon />
+              </p>
             ))}
         </div>
       </div>
 
       <img src={image} alt="" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
